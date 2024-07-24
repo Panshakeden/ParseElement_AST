1b7810df7ece03fd87850607b752f97b8a072252
panshak <panshakeden@gmail.com>
2024-07-24 13:09:00 +0100

    parse elements

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+extern crate scraper;
+
+use scraper::{Html, Selector};
+
+fn parse_html_to_ast(html: &str) -> Html {
+    Html::parse_document(html)
+}
+
+fn print_ast(element: scraper::ElementRef, depth: usize) {
+    let indent = "  ".repeat(depth);
+    println!("{}<{}>", indent, element.value().name());
+
+    for (name, value) in element.value().attrs() {
+        println!("{}  Attribute: {}=\"{}\"", indent, name, value);
+    }
+
+    for child in element.children() {
+        if let Some(_element) = child.value().as_element() {
+            print_ast(scraper::ElementRef::wrap(child).unwrap(), depth + 1);
+        } else if let Some(text) = child.value().as_text() {
+            println!("{}  Text: {:?}", indent, text);
+        }
+    }
+}
+
+fn main() {
+    let html = r#"
+        <html>
+            <head>
+                <title>Parse Element</title>
+            </head>
+            <body>
+                <div class="container">
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                   <p>Helo world</p>
+                </div>
+            </body>
+        </html>
+    "#;
+
+    let html_document = parse_html_to_ast(html);
+    let selector = Selector::parse("html").unwrap();
+
+    for element in html_document.select(&selector) {
+        print_ast(element, 0);
+    }
+}
