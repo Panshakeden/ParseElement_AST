345095fcbc879502114989e27b6dbb0db9a42c29
panshak <panshakeden@gmail.com>
2025-01-14 12:26:06 +0100

    tokenizer

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,32 @@ enum Token{
 }
 
 fn tokenizer(input:&str) -> Vec<Token>{
+    let mut tokens = Vec::new();
+    let mut chars= input.chars().peekable();
 
+    while let Some(&token) = chars.peek(){
+        if token == '<'{
+            chars.next();
+
+            if chars.peek() == Some(&'/'){
+                chars.next();
+
+                let tag_name:String= chars.by_ref().take_while(|&token| token != '>').collect();
+                tokens.push(Token::EndTag(tag_name));
+                chars.next();
+            }else{
+                let tag_name:String = chars.by_ref().take_while(|&token| token != '>' && token != ' ').collect();
+                if chars.peek() == Some(&'>'){
+                tokens.push(Token::StartTag(tag_name));
+                chars.next();    
+                }
+            }
+        }else{
+            let tag_name :String = chars.by_ref().take_while(|&token| token != '<').collect();
+            tokens.push(Token::Text(tag_name));
+        }
+    }
+   tokens
 }
 
 fn main(){
