05afd0c9ecc8705b1d13487f8f8f5eaa9ff7f608
panshak <panshakeden@gmail.com>
2025-01-16 14:02:40 +0100

    parser fn

--- src/main.rs
+++ src/main.rs
@@ -29,13 +29,77 @@ fn tokenizer(input:&str) -> Vec<Token>{
                 }
             }
         }else{
-            let tag_name :String = chars.by_ref().take_while(|&token| token != '<').collect();
+            let tag_name :String = chars.by_ref().take_while(|&tok| tok != '<').collect();
             tokens.push(Token::Text(tag_name));
         }
     }
    tokens
 }
 
+
+#[derive(Debug)]
+
+enum Node{
+    Element{
+        tag_name: String,
+        attributes: Vec<(String,String)>,
+        children: Vec<Node>,
+    },
+    Text(String),
+}
+
+fn parser(tokens:&[Token])-> Node{
+    let mut children = Vec::new();
+    let mut iter = tokens.iter().peekable();
+
+    while let Some(token) = iter.next(){
+          match token{
+            Token::StartTag(tag_name) => {
+                let mut attributes = Vec::new();
+
+                while let Some(Token::Attribute(name,value))= iter.peek(){
+                    attributes.push((name.clone(),value.clone()));
+                    iter.next();
+                }
+
+                let mut nest_child = Vec::new();
+
+                while let Some(next_token) = iter.peek(){
+                    if let Token::EndTag(end_tag) = next_token{
+                        if end_tag == tag_name{
+                            iter.next();
+
+                            break;
+                        }
+                    }
+
+                    nest_child.push(parser(&[iter.next().unwrap().clone()]));
+
+                }
+
+                children.push(Node::Element{
+                    tag_name:tag_name.clone(),
+                    attributes,
+                    children:nest_child,
+                });
+                
+            }
+
+            Token::Text(content) => {
+                children.push(Node::Text(content.clone()));
+            }
+            _ => {}
+          }
+    }
+    Node::Element{
+        tag_name: "root".to_string(),
+        attributes: Vec::new(),
+        children,
+    }
+
+}
+
 fn main(){
+
 
 }
